ad942036038021f1a9a44dcd572d0d8e6832580f
superonesfazai <superonesfazai@gmail.com>
2019-04-27 11:23:24 +0800

    2019-04-27 11:23:24

--- fzutils/exceptions.py
+++ fzutils/exceptions.py
@@ -5,3 +5,16 @@
 @File    : my_exceptions.py
 @connect : superonesfazai@gmail.com
 '''
+
+__all__ = [
+    'ResponseBodyIsNullStrException',       # 请求的应答返回的body为空str异常, 多用于处理proxy异常中, 避免数据误删
+    'NoNextPageException',                  # 没有后续页面的异常
+]
+
+class ResponseBodyIsNullStrException(Exception):
+    """请求的应答返回的body为空str异常, 多用于处理proxy异常中, 避免数据误删"""
+    pass
+
+class NoNextPageException(Exception):
+    """没有后续页面的异常"""
+    pass
