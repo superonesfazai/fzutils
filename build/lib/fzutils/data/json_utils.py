f08f0fbaa555b97bdbabf888de40707c50e3477e
superonesfazai <superonesfazai@gmail.com>
2019-07-31 17:37:32 +0800

    2019-07-31 17:37:32

--- build/lib/fzutils/data/json_utils.py
+++ build/lib/fzutils/data/json_utils.py
@@ -8,12 +8,16 @@
 '''
 
 import re
+from decimal import Decimal
+from datetime import datetime
 
 from ..common_utils import json_2_dict
 
 __all__ = [
     'read_json_from_local_json_file',                       # 从本地json文件读取json, 并以dict返回
     'nonstandard_json_str_handle',                          # 不规范的json_str处理
+    'get_new_list_by_handle_list_2_json_error',             # 处理list转json时, json无法处理某些类型
+    'get_some_type_json_can_recognize',                     # 处理得到json能识别的类型
 ]
 
 def read_json_from_local_json_file(json_file_path):
@@ -47,7 +51,35 @@ def nonstandard_json_str_handle(json_str):
 
     return json_str
 
+def get_new_list_by_handle_list_2_json_error(target_list: (tuple, list)) -> list:
+    """
+    处理list转json时, json无法处理某些类型(测试重写json.JSONEncoder, 实际无法成功处理异常, 故单独转换)
+    :param target_list:
+    :return:
+    """
+    new_item = []
+    for i in target_list:
+        i = get_some_type_json_can_recognize(target=i)
+        new_item.append(i)
 
+    return new_item
+
+def get_some_type_json_can_recognize(target):
+    """
+    处理得到json能识别的类型
+    :param target: dict or list中无法被识别的python类型
+    :return:
+    """
+    if isinstance(target, Decimal):
+        # 处理json无法转decimal
+        target = float(target)
+    elif isinstance(target, datetime):
+        # 处理json无法转datetime
+        target = target.strftime('%Y-%m-%d %H:%M:%S')
+    else:
+        pass
+
+    return target
 
 
 
