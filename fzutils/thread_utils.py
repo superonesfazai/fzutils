cfb7f02ad1c57f30c2563c7608bb7bc643841af1
superonesfazai <superonesfazai@gmail.com>
2019-08-21 13:55:36 +0800

    2019-08-21 13:55:36

--- fzutils/thread_utils.py
+++ fzutils/thread_utils.py
@@ -11,9 +11,14 @@ thread utils
 """
 
 from functools import wraps
+from threading import (
+    Thread,
+)
+from .common_utils import _print
 
 __all__ = [
     'thread_safe',      # 线程安全装饰器
+    'ThreadTaskObj',    # 重写任务线程
 ]
 
 def thread_safe(lock):
@@ -30,3 +35,37 @@ def thread_safe(lock):
         return wrapper
 
     return decorate
+
+class ThreadTaskObj(Thread):
+    """
+    重写任务线程
+    """
+    def __init__(self,
+                 func_name,
+                 args: (list, tuple)=(),
+                 default_res=None,
+                 logger=None):
+        super(ThreadTaskObj, self).__init__()
+        self.func_name = func_name
+        self.args = args
+        # Thread默认结果
+        self.default_res = default_res
+        self.res = default_res
+        self.logger = logger
+
+    def run(self):
+        self.res = self.func_name(*self.args)
+
+    def _get_result(self):
+        try:
+            # 等待线程执行完毕
+            Thread.join(self)
+            return self.res
+        except Exception as e:
+            _print(
+                msg='线程遇到错误:',
+                logger=self.logger,
+                log_level=2,
+                exception=e)
+
+            return self.default_res
